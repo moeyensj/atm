fb1efc977da43172e1aeb12007a257f8fc427b22
Joachim Moeyens <moeyensj@gmail.com>
2019-07-19 09:35:42 -0700

    Add useMedian and statRange arguments to give plotHist more flexibility

--- atm/plotting/histograms.py
+++ atm/plotting/histograms.py
@@ -18,6 +18,9 @@ def plotHist(ax, xValues, xRange,
              bins=10, 
              numGauss=1, 
              robust=True, 
+             swapAxes=False,
+             useMedian=True,
+             statRange=None,
              histKwargs={
                 "histtype" : "stepfilled",
                 "alpha" : 0.5, 
@@ -31,7 +34,14 @@ def plotHist(ax, xValues, xRange,
                 "alpha" : 0.5}):
     
     xValues_cut = xValues[(xValues > xRange[0]) & (xValues < xRange[1])]
+    if statRange is not None:
+        xValues_stat = xValues_cut[(xValues_cut > statRange[0]) & (xValues_cut < statRange[1])]
+    else:
+        xValues_stat = xValues_cut
     print("{} values are outside the defined minimum and maximum.".format(len(xValues) - len(xValues_cut)))
+    
+    if statRange is not None:
+        print("Statistics will be calculated between {} and {}".format(statRange[0], statRange[1]))
     
     ax.hist(xValues, bins=np.linspace(xRange[0], xRange[1], bins), **histKwargs)
     
@@ -40,11 +50,18 @@ def plotHist(ax, xValues, xRange,
         if numGauss == 1:
             stats = np.zeros(3)
             w = 1
-            sigma = calcStdDev(xValues_cut, robust=robust)
-            mu = np.median(xValues_cut)
+            if useMedian is True:
+                mu = np.median(xValues_stat)
+            else:
+                mu = np.mean(xValues_stat)
+                
+            sigma = calcStdDev(xValues_stat, robust=robust)
             grid = np.linspace(*xRange, 1000)
             gauss = norm(mu, sigma).pdf(grid)
-            ax.plot(grid, gauss, **plotKwargs)
+            if swapAxes is True:
+                ax.plot(gauss, grid, **plotKwargs)
+            else:
+                ax.plot(grid, gauss, **plotKwargs)
 
             stats[0] = mu
             stats[1] = sigma
@@ -53,13 +70,16 @@ def plotHist(ax, xValues, xRange,
         else:
             # Fit GMM
             gmm = GaussianMixture(n_components=numGauss, covariance_type="full", tol=0.00000001)
-            gmm = gmm.fit(X=np.expand_dims(xValues_cut, 1))
+            gmm = gmm.fit(X=np.expand_dims(xValues_stat, 1))
 
             # Evaluate GMM
             gmm_x = np.linspace(*xRange, 1000)
             gmm_y = np.exp(gmm.score_samples(gmm_x.reshape(-1, 1))) 
-
-            ax.plot(gmm_x, gmm_y, **plotKwargs)
+            
+            if swapAxes is True:
+                ax.plot(gmm_y, gmm_x, **plotKwargs)
+            else:
+                ax.plot(gmm_x, gmm_y, **plotKwargs)
 
             mu = gmm.means_.ravel()
             sigma = np.sqrt(gmm.covariances_.ravel())
